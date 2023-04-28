9365091ee0c62e9525750c6b68e24b9bec7926cb
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-04-28 20:05:55 +0200

    weekplan functional

--- foodplanner-frontend/src/app/weekplan/weekplan.component.ts
+++ foodplanner-frontend/src/app/weekplan/weekplan.component.ts
@@ -5,7 +5,10 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './weekplan.component.html',
   styleUrls: ['./weekplan.component.scss']
 })
+
 export class WeekplanComponent implements OnInit {
+
+  weekPlan = [{"day": "Montag", "lunch": "Eier", "dinner": "Spaghetti"},{"day": "Dienstag", "dinner": "Pizza"}];
 
   constructor() { }
 
