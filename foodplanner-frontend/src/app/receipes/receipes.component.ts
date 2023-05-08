4331b02b517f2a2b6241a4df4589aa7b90c1eefe
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-05-08 21:12:36 +0200

    comments added to frontend

--- foodplanner-frontend/src/app/receipes/receipes.component.ts
+++ foodplanner-frontend/src/app/receipes/receipes.component.ts
@@ -7,12 +7,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./receipes.component.scss']
 })
 export class ReceipesComponent implements OnInit {
+  // component variables storing information or status
   receipeItems = [{"id": 1, "name":"Spaghetti"}, {"id": 2, "name":"Pizza"}, {"id":3, "name":"Spätzle"}];
   receipeIngredients = [{"name": "Nudeln", "amount": 500, "unit": "gram"}];
   openForm = false;
   showReceipe = false;
   currentReceipe = {"name": "Spaghetti", "description": "fare niente", "id": 0};
   editReceipeMode = false;
+  // Form variables
   receipeName = "";
   receipeDescription = "";
 
@@ -22,6 +24,7 @@ export class ReceipesComponent implements OnInit {
     this.loadReceipes();
   }
 
+  // This function loads the receipes by addressing the get-endpoint for reading all receipes and putting the result to the respective variable
   loadReceipes(){
     const response = this._http.get('http://localhost:5000/receipes')
     .subscribe((res) =>{
@@ -31,12 +34,14 @@ export class ReceipesComponent implements OnInit {
     })
   }
 
+  // This function opens the form template and closes the receipe details, the ingredientslist is emptied to enable a new form.
   openFormFunction(){
     this.openForm = true;
     this.showReceipe = false;
     this.receipeIngredients = [];
   }
 
+  // This function cancels the creation process of a new receipe item by closing the form and resetting the variables and forms
   closeCreation(form1:any, form2:any) {
     this.openForm = false;
     this.receipeIngredients = [];
@@ -44,14 +49,17 @@ export class ReceipesComponent implements OnInit {
     form2.form.reset();
   }
 
+  // This function submits the receipe to create a new one or update an existing one depending on the editReceipeMode flag
   submitReceipe(data:any){
     if (!this.editReceipeMode){
+      // create new receipe
       const response = this._http.post('http://localhost:5000/receipes', {"name":data.form.controls.name.value, "receipe": data.form.controls.receipe.value, "ingredients": this.receipeIngredients}).subscribe(response => {
         this.openForm = false;
         this.loadReceipes();
         data.form.reset()
       });
     } else {
+      // update existing receipe
       const response = this._http.patch('http://localhost:5000/receipes/'+this.currentReceipe.id, {"name":data.form.controls.name.value, "receipe": data.form.controls.receipe.value, "ingredients": this.receipeIngredients}).subscribe(response => {
         this.openForm = false;
         this.editReceipeMode = false;
@@ -61,6 +69,7 @@ export class ReceipesComponent implements OnInit {
     }
   }
 
+  // this function calls the api to get the details of a receipe, saves them to the corresponding variables, closes the input form and opens the receipe form
   openReceipe(receipe:number) {
     const response = this._http.get('http://localhost:5000/receipes/'+receipe)
     .subscribe((res) =>{
@@ -68,11 +77,12 @@ export class ReceipesComponent implements OnInit {
       let resultJSON = JSON.parse(resultString);
       this.currentReceipe = resultJSON.receipe;
       this.receipeIngredients = resultJSON.ingredients;
-      this.showReceipe = true;  
       this.openForm = false;
+      this.showReceipe = true;  
     })
   }
 
+  // this function closes the receipe view and resets the corresponding variables
   closeReceipe(){
     this.openForm = false;
     this.showReceipe = false;
@@ -80,12 +90,14 @@ export class ReceipesComponent implements OnInit {
     this.receipeIngredients = [];
   }
 
+  // this function adds ingredients to the CLIENT list of ingredients, it doesn't save it to the backend
   submitIngredient(data:any){
     const ingredientToAdd = {"name": data.form.controls.name.value, "amount": data.form.controls.amount.value, "unit": data.form.controls.unit.value};
     this.receipeIngredients.push(ingredientToAdd);
     data.form.reset()
   }
 
+  // this function calls the api to delete a specific receipe
   deleteReceipe(){
     const response = this._http.delete('http://localhost:5000/receipes/'+this.currentReceipe.id)
     .subscribe((res)=> {
@@ -94,6 +106,7 @@ export class ReceipesComponent implements OnInit {
     })
   }
 
+  // this fucntion drops an ingredient of the CLIENTs ingredientlist, not from the backend
   dropIngredient(name:String) {
       this.receipeIngredients.forEach((element, index) => {
         if (element.name == name) {
@@ -103,6 +116,7 @@ export class ReceipesComponent implements OnInit {
       })
   }
 
+  // this fucntion starts the editing mode for a receipe
   editReceipe(){
     this.showReceipe = false;
     this.openForm = true;
