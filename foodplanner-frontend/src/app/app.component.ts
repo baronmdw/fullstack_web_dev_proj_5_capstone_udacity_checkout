22d0379a34f2c49b1cec04a62fdb27077cadb884
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-05-14 17:31:10 +0200

    packages from udacity input work

--- foodplanner-frontend/src/app/app.component.ts
+++ foodplanner-frontend/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Platform } from '@ionic/angular';
 
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,25 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent {
   title = 'foodplanner-frontend';
   responsedata:any;
-  constructor(private _http:HttpClient){}
+  constructor(
+    private _http:HttpClient,
+    private auth: AuthService,
+    private platform: Platform,
+    ){
+      this.initializeApp();
+    }
 
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // this.statusBar.styleDefault();
+      // this.splashScreen.hide();
+
+      // Perform required auth actions
+      this.auth.load_jwts();
+      this.auth.check_token_fragment();
+    });
+  }
   connectionTest() {
     const response = this._http.get('http://localhost:5000/')
     .subscribe(res =>{
