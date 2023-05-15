17b517c67c9f25c4d84473d40913aa3de066f790
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-05-15 21:25:25 +0200

    adapted env.prod

--- foodplanner-frontend/src/environments/environment.prod.ts
+++ foodplanner-frontend/src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    url: process.env["url"], // the auth0 domain prefix
+    audience: process.env["audience"], // the audience set for the auth0 app
+    clientId: process.env["clientId"], // the client id generated for the auth0 app
+    callbackURL: process.env["callbackURL"], // the base url of the running ionic application. 
+  },
+  apiServerUrl: process.env["apiServerUrl"]
 };
