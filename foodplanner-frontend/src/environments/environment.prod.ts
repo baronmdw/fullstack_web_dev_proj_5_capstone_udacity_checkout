2b434e292302deeda1f80c25adf2f517c276d38c
baronmdw <58744855+baronmdw@users.noreply.github.com>
2023-05-15 22:49:23 +0200

    Update environment.prod.ts

--- foodplanner-frontend/src/environments/environment.prod.ts
+++ foodplanner-frontend/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
     url: require("../../auth_config.json").url, // the auth0 domain prefix
     audience: require("../../auth_config.json").audience, // the audience set for the auth0 app
     clientId: require("../../auth_config.json").clientId, // the client id generated for the auth0 app
-    callbackURL: require("../../auth_config.json").callbackURL, // the base url of the running ionic application. 
+    callbackURL: "https://foodplanner-frontend.onrender.com", // the base url of the running ionic application. 
   },
   apiServerUrl: "https://foodplanner-backend.onrender.com",
 };
